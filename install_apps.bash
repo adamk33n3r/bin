1f816935dab72c3552c4ccd4c22f36911d4b70f2
Adam Keenan <adam_keenan@taylor.edu>
2014-03-06 19:47:56 +0000

    Installing general apps and setting up websurv enviro

---
+++ install_apps.bash
@@ -0,0 +1,2 @@
+sudo apt-get install git vim emacs24 virtualenvwrapper
+. /etc/bash_completion.d/virtualenvwrapper
---
+++ set_up_websurv.bash
@@ -0,0 +1,3 @@
+git clone git@github.com:tu-software-studio/websurv.git
+
+mkvirtualenv django -a ~/websurv -r ~/websurv/requirements.txt

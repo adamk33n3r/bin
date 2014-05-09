5ed8dc10a0635da3b53b2453a45e147213af432b
Adam Keenan <adam_keenan@taylor.edu>
2014-05-09 07:20:11 +0000

    Added download for git-completion script

--- install_configs.bash
+++ install_configs.bash
@@ -1,4 +1,7 @@
 set -x
 cp -r .vim/ .emacs.d/ ~/
 cp .vimrc .gitconfig .bash* ~/
+wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+mv git-completion.bash ~/.git-completion.bash
+
 . ~/.bashrc
